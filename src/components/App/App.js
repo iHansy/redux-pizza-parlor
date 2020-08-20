34cf4f87c407c7f030ec0ffa459b2b16fa574cb9
Lara Lesar <lara.m.lesar@gmail.com>
2020-08-20 16:25:52 -0500

    first commit inputs done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import CustomerInfo from '../CustomerInfo/CustomerInfo.js'
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <br/>
         <img src="images/pizza_photo.png"/>
         <p>Pizza is great.</p>
+        <CustomerInfo />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+
+
+//store all pizzas currently in cart
+const cartReducer =(state = [], action) => {
+    return state
+}
+
+const customerReducer =(state, action) => {
+    return state
+}
+const pizzaListReducer =(state, action) => {
+    return state
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
