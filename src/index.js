404e718db6118551aefece037f89cd82f6b78f5d
Nick Evans <nickevans20@outlook.com>
2020-08-20 14:26:17 -0500

    Made ListOfPizzas Component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
