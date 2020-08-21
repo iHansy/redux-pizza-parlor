aa22a751387c84fca8a8dc2963f7549393d5b00b
Hans Accola <Hansolo6283@gmail.com>
2020-08-21 09:18:58 -0500

    working on displaying customerinfo on DOM

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import CustomerToCheckout from '../CustomerToCheckout/CustomerToCheckout';
 
 class Checkout extends Component {
 
+
     componentDidMount() {
         this.getCustomers();
-      }
-    
-      getCustomers = () => {
+    }
+
+    getCustomers = () => {
         //request to database
         axios({
-          method: 'GET',
-          url: '/api/order'
+            method: 'GET',
+            url: '/api/order'
         }).then((response) => {
             console.log(response.data);
             //response.data will be array of customer info
@@ -21,16 +23,44 @@ class Checkout extends Component {
         }).catch((error) => {
             console.log('error getting customer info', error);
         })
-      }
-
-
-
+    }
 
 
     render() {
+        // let customerIndex = this.props.currentCustomer.length - 1
+        // console.log(customerIndex);
+        //console.logs can go here
+        // console.log(this.props.reduxStore.customerReducer[1].customer_name);
+        // console.log(this.props.currentCustomer[customerIndex])
+
+        // let customer = this.props.storeCustomers.length
+
+        // let customerPass = customer - 1
+
+        console.log(this.props.storeCustomers.length - 1);
+        let startingIndex = this.props.storeCustomers.length - 1
+        console.log(startingIndex);
+
+        for (let i = 3; i < this.props.storeCustomers.length; i++ ) {
+            console.log(i);
+            console.log(this.props.storeCustomers[i].customer_name);
+            // console.log(this.props.storeCustomers;
+            // console.log(this.props.storeCustomers[i].customer_name);
+
+            //double ampersand?
+
+
+        }
+
         return (
             <div>
                 <h3>Step 3: Checkout</h3>
+                {/* <CustomerToCheckout customer={this.props.storeCustomers[this.props.storeCustomers.length - 1]}/> */}
+                {/* {this.props.reduxStore.currentCustomer.map((customer, index) => {
+                    return (
+                        <p>{customer.id}</p>
+                    )
+                })} */}
             </div>
         )
     }
@@ -38,8 +68,8 @@ class Checkout extends Component {
 
 const mapStateToProps = (reduxStore) => {
     return {
-        reduxStore
+        storeCustomers: reduxStore.customerReducer
     }
 }
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps)(Checkout); //connecting with redux to receive and send from
